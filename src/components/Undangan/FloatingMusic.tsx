93e031ecf542d16f154593bbc88bb7582b945795
Ravi <razkhan070504@gmail.com>
2026-08-05 21:50:46 +0700

    pan

--- src/components/Undangan/FloatingMusic.tsx
+++ src/components/Undangan/FloatingMusic.tsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
 import { Music, Pause } from 'lucide-react';
 import { motion } from 'motion/react';
-import bgsound from '/assets/bg-sound.mp3';
+import bgsound from "/assets/bg-sound.mp3";
 
 export default function FloatingMusic() {
   const [isPlaying, setIsPlaying] = useState(false);
