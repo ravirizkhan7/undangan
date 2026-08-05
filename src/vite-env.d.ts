28772914fcec753ec66c48213baa57edc5485f9b
Ravi <razkhan070504@gmail.com>
2026-08-05 17:07:48 +0700

    as

---
+++ src/vite-env.d.ts
@@ -0,0 +1,16 @@
+// <reference types="vite/client" />
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
