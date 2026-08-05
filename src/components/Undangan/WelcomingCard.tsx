95a802c31425a188510cdff11274d8463c760d79
Ravi <razkhan070504@gmail.com>
2026-08-05 17:25:35 +0700

    cok

--- src/components/Undangan/WelcomingCard.tsx
+++ src/components/Undangan/WelcomingCard.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'motion/react';
-import wallpaper from './assets/img/3.jpeg';
+import wallpaper from './assets/img/3.JPEG';
 
 interface WelcomingCardProps {
   onOpen: () => void;
